4ef9b13e1fd8b601c39b4c1695205c45840abd71
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2024-07-25 11:08:25 -0300

    style: fix strange behavior on mobile

--- src/components/ui/FlipWords.tsx
+++ src/components/ui/FlipWords.tsx
@@ -2,6 +2,7 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "@/lib/utils";
+import { useMediaQuery } from 'react-responsive';
 
 export const FlipWords = ({
   words,
@@ -14,6 +15,7 @@ export const FlipWords = ({
 }) => {
   const [currentWord, setCurrentWord] = useState(words[0]);
   const [isAnimating, setIsAnimating] = useState<boolean>(false);
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
 
   const startAnimation = useCallback(() => {
     const word = words[words.indexOf(currentWord) + 1] || words[0];
@@ -56,7 +58,7 @@ export const FlipWords = ({
           x: 40,
           filter: "blur(8px)",
           scale: 2,
-          position: "absolute",
+          position: isMobile ? "fixed" : "absolute",
         }}
         className={cn(
           "z-10 inline-block relative text-left text-neutral-900 dark:text-neutral-100 px-2",
